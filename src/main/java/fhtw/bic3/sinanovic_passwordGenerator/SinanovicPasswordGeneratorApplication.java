94402a664e5bb007fb83308e542255becf0f8e41
sinanovice <ic22b019@technikum-wien.at>
2023-01-30 17:57:38 +0100

    Initial commit

---
+++ src/main/java/fhtw/bic3/sinanovic_passwordGenerator/SinanovicPasswordGeneratorApplication.java
@@ -0,0 +1,13 @@
+package fhtw.bic3.sinanovic_passwordGenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SinanovicPasswordGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SinanovicPasswordGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fhtw/bic3/sinanovic_passwordGenerator/SinanovicPasswordGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package fhtw.bic3.sinanovic_passwordGenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SinanovicPasswordGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
